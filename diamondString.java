1c3aac917864af83590821a704ae34ebf4895528
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-06-07 23:32:42 -0400

    even smaller

--- diamondString.java
+++ diamondString.java
@@ -1,38 +1,21 @@
-// Aaron Prince Anu
-
 public class diamondString {
     public static void main(String[] args) {
         while (true) {
-            System.out.print("\nEnter a word (or type 'exit' to quit): ");
+            System.out.print("\nEnter a word (or type 'exit' to quit): \n");
             String input = In.getString();
 
-            if (input.equalsIgnoreCase("exit")) {
-                System.out.println("\nExiting ...\n");
-                break;
-            } else if (input.length() > 10) {
-                System.out.println("\nInput word is too long. Please enter another word.");
-                continue;
-            }
+            if (input.equalsIgnoreCase("exit")) break;
+            if (input.length() > 10) { System.out.println("\nInput word is too long. Please enter another word.\n"); continue; }
 
-            int length = input.length();
-            int totalRows = (length * 2) - 1;
-            int midRow = totalRows / 2;
+            int n = input.length() * 2 - 1, m = n / 2;
 
-            for (int row = 0; row < totalRows; row++) {
-                int spaces = Math.abs(midRow - row);
-                print(" ".repeat(spaces));
-                print(input.substring(0, length - spaces));
-                print(new StringBuilder(input.substring(0, length - spaces - 1)).reverse().toString());
-                println();
+            for (int i = 0; i < n; i++) {
+                int spaces = Math.abs(m - i);
+                System.out.print(" ".repeat(spaces));
+                System.out.print(input.substring(0, input.length() - spaces));
+                System.out.print(new StringBuilder(input.substring(0, input.length() - spaces - 1)).reverse().toString());
+                System.out.println();
             }
         }
-    }
-    
-    public static void print(String s) {
-        System.out.print(s);
-    }
-    
-    public static void println() {
-        System.out.println();
     }
 }
